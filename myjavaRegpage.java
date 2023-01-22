bf5fb7b17a015c5a8b40a3e80236f45ee3d09172
kancha2017”git config --global user.email kanchanlule2017@gmail.com” <kanchanlule2017@gmail.comgit config --global user.name kancha2017>
2023-01-22 22:14:18 +0530

    this is my java reg page

--- myjavaRegpage.java
+++ myjavaRegpage.java
@@ -1,18 +1,1 @@
-1st line of line java devoloper A
-
-2nd line by devoloper B
-
-
-3rd line DEV A
-
-4th line Dev B
-
-
-
-
-
-
-
-this is my java Reg page 
-
-i am kanchan 
+this is my java registration page
